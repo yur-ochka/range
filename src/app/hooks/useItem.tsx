6043510221b3ddbd7e6ee23056bebd0d7e8c7cdc
Illia Yurchenko <iliayurchenko06@gmail.com>
2025-10-27 21:29:02 +0200

    Add detailed item page

--- src/app/hooks/useItem.tsx
+++ src/app/hooks/useItem.tsx
@@ -1,22 +1,13 @@
-import { useQuery } from "@tanstack/react-query";
-import api from "../lib/api/axiosInstance";
-export type Item = {
-  id: string;
-  title: string;
-  subtitile: string | null;
-  description: string | null;
-  price: number;
-  imageUrl: string | null;
-  createdAt: string;
-  updatedAt: string;
-};
-export function useItem(id: string) {
-  return useQuery<Item>({
-    queryKey: ["item", id],
-    queryFn: async () => {
-      const response = await api.get(`/api/items/${id}`);
-      return response.data;
-    },
-    retry: false,
-  });
-}
+// import { useQuery } from "@tanstack/react-query";
+// import api from "../lib/api/axiosInstance";
+
+// export function useItem(id: string) {
+//   return useQuery<Item>({
+//     queryKey: ["item", id],
+//     queryFn: async () => {
+//       const response = await api.get(`/api/items/${id}`);
+//       return response.data;
+//     },
+//     retry: false,
+//   });
+// }
