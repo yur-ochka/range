db81bb6a4c2524c614fcdd30eb185f90865bf7b5
Kyryl <kirillka.k@gmail.com>
2025-10-28 19:15:55 +0200

    refactor: renamed accounts to user

--- backend/monolith/core/settings.py
+++ backend/monolith/core/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     # Local apps (to be added as you migrate)
-    # 'apps.accounts',
+    # 'apps.user',
     # 'apps.catalog',
     # 'apps.cart',
     # 'apps.order',
--- backend/monolith/core/urls.py
+++ backend/monolith/core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/accounts/', include('apps.accounts.urls')),
+    # path('api/users/', include('apps.users.urls')),
     # path('api/catalog/', include('apps.catalog.urls')),
     # path('api/cart/', include('apps.cart.urls')),
     # path('api/order/', include('apps.order.urls')),
