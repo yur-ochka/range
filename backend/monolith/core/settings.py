d4a7ab0c33647eab28bd3f4ab396eace9db04806
Kyryl <kirillka.k@gmail.com>
2025-12-07 19:39:56 +0200

    fix: fixxed error with static

--- backend/monolith/core/settings.py
+++ backend/monolith/core/settings.py
@@ -272,7 +272,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (you can enable WhiteNoise if desired)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
