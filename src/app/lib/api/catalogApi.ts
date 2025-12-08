5a5836f7c42778db7937fb43a7f627717f06388e
MelArt24 <animator2404@gmail.com>
2025-12-08 12:10:30 +0200

    Revert "update readme.md"
    This reverts commit 8687ccf816dcee7d9dd7257cda8cbea3cdbc008d.

--- src/app/lib/api/catalogApi.ts
+++
@@ -1,7 +0,0 @@
-import api from "./axiosInstance";
-import type { Category } from "@/types/catalog";
-
-export async function getCategories(): Promise<{ results: Category[] }> {
-    const res = await api.get("/catalog/categories/");
-    return res.data;
-}
